7f6eb207b6297baf33e3674974c93f9bbff7bb83
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2024-07-18 21:17:29 +0530

    Time: 3 ms (45.60%) | Memory: 43.1 MB (52.45%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,31 +1,32 @@
+import java.util.*;
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
         List<Integer> list = new ArrayList<>();
-        int firstIndex = binarySearch(nums, target);
-        if (firstIndex == -1) {
+        Arrays.sort(nums);
+        int firstOcc = findFirstOcc(nums, target);
+        if(firstOcc==-1) {
             return list;
         }
-        for (int i = firstIndex; i < nums.length && nums[i] == target; i++) {
+        for(int i=firstOcc; i<nums.length && nums[i] == target; i++) {
             list.add(i);
         }
-        
         return list;
+
     }
-    public int binarySearch(int[] nums, int k) {
-        int low=0, high= nums.length-1;
+    public int findFirstOcc(int[] nums, int key) {
+        int low = 0, high = nums.length-1;
         int mid = low + (high-low)/2;
         int ans=-1;
         while(low<=high) {
-            if(nums[mid]==k) {
+            if(nums[mid]==key) {
                 ans=mid;
                 high=mid-1;
             }
-            else if(k>nums[mid]) {
+            else if(key>nums[mid]) {
                 low=mid+1;
             }
-            else if(k<nums[mid]) {
-                high = mid-1;
+            else {
+                high=mid-1;
             }
             mid = low + (high-low)/2;
         }
