ac56dbeb978e9cbe02fe39ce59bcf79c8b48c58d
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2024-08-28 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (85.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result= new int[2];
-        result[0] = firstPos(nums, target);
-        result[1] = lastPos(nums, target);
-        return result;
-    }
-    public int firstPos(int[] nums, int k) {
-        int low=0, high= nums.length-1;
-        int mid = low + (high-low)/2;
-        int ans=-1;
-        while(low<=high) {
-            if(nums[mid]==k) {
-                ans=mid;
-                high=mid-1;
-            }
-            else if(k>nums[mid]) {
-                low=mid+1;
-            }
-            else if(k<nums[mid]) {
-                high = mid-1;
-            }
-            mid = low + (high-low)/2;
-        }
+        int low = 0, high = nums.length-1;
+        int mid= low+(high-low)/2;
+        int[] ans = new int[2];
+        ans[0] = firstPos(nums, low, high, target);
+        ans[1] = lastPos(nums, low, high, target);
         return ans;
     }
-    public int lastPos(int[] nums, int k) {
-        int low=0, high= nums.length-1;
-        int mid = low + (high-low)/2;
-        int ans=-1;
-        while(low<=high) {
-            if(nums[mid]==k) {
-                ans=mid;
-                low=mid+1;
-            }
-            else if(k>nums[mid]) {
-                low=mid+1;
-            }
-            else if(k<nums[mid]) {
-                high = mid-1;
-            }
-            mid = low + (high-low)/2;
+    public int firstPos(int[] nums, int low, int high, int target) {
+        if(low>high) {
+            return -1;
         }
-        return ans;
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target) {
+            int firstOcc = firstPos(nums, low, mid-1, target);
+            if(firstOcc == -1) {
+                return mid;
+            }
+            else {
+                return firstOcc;
+            }
+        }
+        else if(target > nums[mid]) {
+            return firstPos(nums, mid+1, high, target);
+        }
+        else {
+            return firstPos(nums, low, mid-1, target);
+        }
+    }
+    public int lastPos(int[] nums, int low, int high, int target) {
+        if(low>high) {
+            return -1;
+        }
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target) {
+            int lastOcc = lastPos(nums, mid+1, high, target);
+            if(lastOcc == -1) {
+                return mid;
+            }
+            else {
+                return lastOcc;
+            }
+        }
+        else if(target > nums[mid]) {
+            return lastPos(nums, mid+1, high, target);
+        }
+        else {
+            return lastPos(nums, low, mid-1, target);
+        }
     }
 }
