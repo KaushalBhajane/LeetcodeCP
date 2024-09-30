a0c0f4748c6e24cbb5b910e0088bb172e49c1b7f
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2024-09-30 18:37:18 +0530

    Time: 1 ms (27.40%) | Memory: 41.7 MB (22.27%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,48 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
-            return 0;
-        }
-        
-        int haystackLength = haystack.length();
-        int needleLength = needle.length();
-        
-        // We only need to check up to haystack.length() - needle.length()
-        for (int i = 0; i <= haystackLength - needleLength; i++) {
-            // Compare the substring of length `needleLength` starting at `i`
-            if (haystack.substring(i, i + needleLength).equals(needle)) {
-                return i; // Return the starting index of the match
+        int[] lps = lps(needle);
+        int first=0, second=0;
+        while(first<haystack.length() && second<needle.length()) {
+            if(haystack.charAt(first) == needle.charAt(second)) {
+                first++;
+                second++;
+            }
+            else {
+                if(second==0) {
+                    first++;
+                }
+                else {
+                    second=lps[second-1];
+                }
             }
         }
-        
-        return -1; // No match found
+        if(second==needle.length()) {
+            return first-second;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public static int[] lps(String needle) {
+        int[] lps = new int[needle.length()];
+        int pref=0, suff=1;
+        while(suff<needle.length()) {
+            if(needle.charAt(pref) == needle.charAt(suff)) {
+                lps[suff] = pref+1;
+                pref++;
+                suff++;
+            }
+            else {
+                if(pref==0) {
+                    lps[suff]=0;
+                    suff++;
+                }
+                else {
+                    pref = lps[pref-1];
+                }
+            }
+        }
+        return lps;
     }
 }
