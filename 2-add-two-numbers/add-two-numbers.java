587c008f43cf4a00f1d1b88f109f300fba4cb6ff
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2024-10-31 16:15:22 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (55.60%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -45,16 +45,5 @@ class Solution {
         
         return head.next;
     }
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = null;
-        while(curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+    
 }
