efb989f012cc992762746a99a3a2e6e15e39271e
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2024-10-31 16:14:59 +0530

    Time: 2 ms (15.87%) | Memory: 44.3 MB (70.26%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode curr1 = l1;
+        ListNode curr2 = l2;
+        ListNode head = new ListNode(-1);
+        ListNode tail = head;
+        int carry=0, sum=-1;
+        while(curr1 != null && curr2 != null) {
+            sum = curr1.val + curr2.val + carry;
+            tail.next = new ListNode(sum%10);
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+            tail = tail.next;
+            carry = sum/10;
+        }
+        while(curr1 != null) {
+            sum = curr1.val + carry;
+            tail.next = new ListNode(sum%10);
+            curr1=curr1.next;
+            tail = tail.next;
+            carry = sum/10;
+        }
+        while(curr2 != null) {
+            sum = curr2.val + carry;
+            tail.next = new ListNode(sum%10);
+            curr2=curr2.next;
+            tail = tail.next;
+            carry = sum/10;
+        }
+        while(carry != 0) {
+            tail.next = new ListNode(carry%10);
+            tail = tail.next;
+            carry = carry/10;
+        }
+        
+        return head.next;
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
