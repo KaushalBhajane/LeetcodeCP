12c34a73356ee4b527952c569021588a68d3ae17
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2025-01-25 17:02:40 +0530

    Time: 32 ms (45.48%) | Memory: 51.4 MB (71.87%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,23 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
-        Set<List<Integer>> outerList = new HashSet<>();
-        for(int i=0; i<n-2; i++) {
-            Set<Integer> uniqueEle = new HashSet<>();
-            for(int j=i+1; j<n; j++) {
-                int comp = -nums[i]-nums[j];
-                if(uniqueEle.contains(comp)) {
-                    List<Integer> innerList = new ArrayList<>();
-                    innerList.add(comp);
-                    innerList.add(nums[i]);
-                    innerList.add(nums[j]);
-                    innerList.sort(Comparator.reverseOrder()); // or sort(null) coz in set of lists ([-1,1,0], [-1,0,1]) are diff due to order.
-                    outerList.add(innerList);
-                }
-                uniqueEle.add(nums[j]);
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-            
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int current_sum = nums[left] + nums[right];
+                if (current_sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (current_sum < target) {
+                    left++;
+                } else if (current_sum > target) {
+                    right--;
+                }
+            }
         }
-        return new ArrayList<>(outerList);
+        return result;
     }
 }
