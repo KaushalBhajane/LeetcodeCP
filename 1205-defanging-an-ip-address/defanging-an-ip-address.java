01eed9756a90ab0608e35f796094bdffafd441a7
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2025-01-27 20:23:09 +0530

    Time: 1 ms (36.94%) | Memory: 42.1 MB (9.37%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,15 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String temp = "";
-        int index = 0;
-        while (index < address.length()) {
-            if (address.charAt(index) == '.') { // Check characters from address, not temp
-                temp += "[.]"; // Add "[.]" when a dot is encountered
-            } else {
-                temp += address.charAt(index); // Add the current character
-            }
-            index++;
-        }
-        return temp;
+        String[] str1 = address.split("\\.");
+        return String.join("[.]", str1);
     }
 }
