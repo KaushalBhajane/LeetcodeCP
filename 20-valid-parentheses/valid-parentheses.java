3c55c78eaa25a628f55f7bb79b948f30bb5d0c56
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2025-06-25 21:58:37 +0530

    Time: 4 ms (11.91%) | Memory: 41.8 MB (65.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char curr = s.charAt(i);
+            if(!stack.isEmpty()) {
+                char last = stack.peek();
+                if(last=='(' && curr==')' || last=='[' && curr==']' || last=='{' && curr=='}') {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(s.charAt(i));
+        }
+        return stack.isEmpty();
+    }
+}
