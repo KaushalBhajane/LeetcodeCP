ccd1b85c529639f70f2540f610d47f14c0c51f79
KaushalBhajane <131263768+KaushalBhajane@users.noreply.github.com>
2025-08-12 18:05:46 +0530

    Time: 2 ms (96.24%) | Memory: 46.1 MB (33.00%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode balanceBST(TreeNode root) {
+        // Add your code here.
+        
+        ArrayList<Integer> sortedNodes = new ArrayList<>();
+        inorder(root, sortedNodes);
+        
+        TreeNode newRoot = balancedTree(sortedNodes, 0, sortedNodes.size()-1);
+        return newRoot;
+    }
+    
+    void inorder(TreeNode root, ArrayList<Integer> sortedNodes) {
+        if(root == null) {
+            return;
+        }
+        
+        inorder(root.left, sortedNodes);
+        sortedNodes.add(root.val);
+        inorder(root.right, sortedNodes);
+    }
+    
+    TreeNode balancedTree(ArrayList<Integer> sortedNodes, int start, int end) {
+        if(start>end) {
+            return null;
+        }
+        int mid = start + (end-start)/2;
+        TreeNode root = new TreeNode(sortedNodes.get(mid));
+        
+        root.left = balancedTree(sortedNodes, start, mid-1);
+        root.right = balancedTree(sortedNodes, mid+1, end);
+        
+        return root;
+    }
+}
